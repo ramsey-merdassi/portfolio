2735e90a776f0aafc1e35c8e759f72b7d033be5c
Ramsey Merdassi <merdassi.r@gmail.com>
2025-05-06 14:13:08 +0100

    add all projects

--- carousel.js
+++ carousel.js
@@ -1,23 +1,64 @@
-const image = document.querySelector(".project-img")
-const leftArrow = document.querySelector('.bx-chevron-left')
-const rightArrow = document.querySelector('.bx-chevron-right')
+const images = document.querySelectorAll(".project-img")
+const leftArrows = document.querySelectorAll('.bx-chevron-left')
+const rightArrows = document.querySelectorAll('.bx-chevron-right')
 
-leftArrow.addEventListener('click', () => {
-    if(image.src.match('collector_app1')) {
-        image.src = 'images/collector_app3.png'
-    } else if (image.src.match('collector_app3')) {
-        image.src = 'images/collector_app2.png'
-    } else if (image.src.match('collector_app2')){
-        image.src = 'images/collector_app1.png'
+function scrollImages (img1, img2, img3) {
+    for (let i = 0; i < images.length; i++) {
+        leftArrows[i].addEventListener('click', () => {
+            if (images[i].src.includes(img1)) {
+                images[i].src = img3
+            } else if (images[i].src.includes(img2)) {
+                images[i].src = img1
+            } else if (images[i].src.includes(img3)) {
+                images[i].src = img2
+            }
+        })
+        rightArrows[i].addEventListener('click', () => {
+            if (images[i].src.includes(img1)) {
+                images[i].src = img2
+            } else if (images[i].src.includes(img2)) {
+                images[i].src = img3
+            } else if (images[i].src.includes(img3)) {
+                images[i].src = img1
+            }
+        })
     }
-})
+}
+scrollImages('images/collector_app1.png', 'images/collector_app2.png', 'images/collector_app3.png')
+scrollImages('images/film_quiz1.png', 'images/film_quiz2.png', 'images/film_quiz3.png')
+scrollImages('images/job_listing1.png', 'images/job_listing2.png', 'images/job_listing3.png')
+scrollImages('images/furniture_store1.png', 'images/furniture_store2.png', 'images/furniture_store3.png')
+scrollImages('images/academy_portal1.png', 'images/academy_portal2.png', 'images/academy_portal3.png')
+scrollImages('images/journey_planner1.png', 'images/journey_planner2.png', 'images/journey_planner3.png')
 
-rightArrow.addEventListener('click', () => {
-    if(image.src.match('collector_app1')) {
-        image.src = 'images/collector_app2.png'
-    } else if (image.src.match('collector_app2')) {
-        image.src = 'images/collector_app3.png'
-    } else if (image.src.match('collector_app3')) {
-        image.src = 'images/collector_app1.png'
-    }
-})
+
+// function scrollImagesMobile (img1, img2, img3) {
+//     for (let i = 0; i < images.length; i++) {
+//         images[i].addEventListener('ontouchmove', () => {
+//             if (images[i].src.includes(img1)) {
+//                 images[i].src = img3
+//             } else if (images[i].src.includes(img2)) {
+//                 images[i].src = img1
+//             } else if (images[i].src.includes(img3)) {
+//                 images[i].src = img2
+//             }
+//         })
+//         images[i].addEventListener('ontouchmove', () => {
+//             if (images[i].src.includes(img1)) {
+//                 images[i].src = img2
+//             } else if (images[i].src.includes(img2)) {
+//                 images[i].src = img3
+//             } else if (images[i].src.includes(img3)) {
+//                 images[i].src = img1
+//             }
+//         })
+//     }
+// }
+// scrollImagesMobile('images/collector_app1.png', 'images/collector_app2.png', 'images/collector_app3.png')
+// scrollImagesMobile('images/film_quiz1.png', 'images/film_quiz2.png', 'images/film_quiz3.png')
+// scrollImagesMobile('images/job_listing1.png', 'images/job_listing2.png', 'images/job_listing3.png')
+// scrollImagesMobile('images/furniture_store1.png', 'images/furniture_store2.png', 'images/furniture_store3.png')
+// scrollImagesMobile('images/academy_portal1.png', 'images/academy_portal2.png', 'images/academy_portal3.png')
+// scrollImagesMobile('images/journey_planner1.png', 'images/journey_planner2.png', 'images/journey_planner3.png')
+
+
